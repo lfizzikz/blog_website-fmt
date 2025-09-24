5fff03c0d8b7a1b001e8cd78c408bfccbbf911b3
Trevor Nance <trevornance7@gmail.com>
2025-09-24 18:38:37 -0400

    added some error handling. email logging for error

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from urllib.parse import urlsplit
 
 import sqlalchemy as sa
@@ -5,7 +6,7 @@ from flask import flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required, login_user, logout_user
 
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import EditProfileForm, LoginForm, RegistrationForm
 from app.models import User
 
 
@@ -16,7 +17,7 @@ def index():
     user = {"username": "Miguel"}
     posts = [
         {"author": {"username": "John"}, "body": "Hey Everyone"},
-        {"author": {"username": "Susan"}, "body": "David Sucks!"},
+        {"author": {"username": "Laura"}, "body": "David Sucks!"},
     ]
     return render_template("index.html", title="Home", posts=posts)
 
@@ -71,3 +72,26 @@ def user(username):
         {"author": user, "body": "Test post #2"},
     ]
     return render_template("user.html", user=user, posts=posts)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.now(timezone.utc)
+        db.session.commit()
+
+
+@app.route("/edit_profile", methods=["GET", "POST"])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash("Your changes have been saved.")
+        return redirect(url_for("edit_profile"))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template("edit_profile.html", title="Edit Profile", form=form)
